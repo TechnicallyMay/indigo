89d1c1c1066519a69850512c5552cf70f77dc424
TechnicallyMay <masonwells01@gmail.com>
2025-11-24 21:14:20 -0700

    SMTP

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"net/smtp"
 	"regexp"
 
 	"codeberg.org/go-pdf/fpdf"
@@ -15,12 +17,32 @@ var validPath = regexp.MustCompile("^(/[a-zA-Z0-9]+/{0,1}?)*$")
 func main() {
 	log.Println("Starting!")
 
+	// PDF
 	log.Println("making example pdf")
 	pdf := fpdf.New("P", "mm", "A4", "")
 	pdf.AddPage()
 	pdf.SetFont("Arial", "B", 16)
 	pdf.Cell(40, 10, "you owe me so much money")
 	pdf.OutputFileAndClose("hello.pdf")
+
+	// MAIL
+	log.Println("Sending an email")
+	host := "smtp.mailgun.org"
+	addr := host + ":587"
+	var smtpPass string
+	fmt.Println("Enter the smtp pass")
+	fmt.Scan(&smtpPass)
+	auth := smtp.PlainAuth("", "postmaster@sandbox6799805213174515aa97c14ef663c50e.mailgun.org", smtpPass, host)
+
+	msg := []byte("From: postmaster@sandbox6799805213174515aa97c14ef663c50e.mailgun.org\r\n" +
+		"To: masonwells01@gmail.com\r\n" +
+		"Subject: Test mail\r\n\r\n" +
+		"Email body\r\n")
+	mail := Multipart
+	err := smtp.SendMail(addr, auth, "mail@chickpea-home.duckdns.org", []string{"masonwells01@gmail.com"}, msg)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	pool := db.OpenDb()
 	defer pool.Close()
